43d0de101561e04b69b2ea98773df3f01fbc1e3a
Sahay-Snehil <strange.snehil22@icloud.com>
2026-08-31 00:23:05 +0530

    Added

--- src/main/java/com/restaurant/controller/HelloController.java
+++ src/main/java/com/restaurant/controller/HelloController.java
@@ -1,8 +1,7 @@
 package com.restaurant.controller;
 
-import java.util.List;
 import java.util.ArrayList;
-
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +13,12 @@ public class HelloController {
     public String hello() {
         return "Hello World!";
     }
+
     @GetMapping("/")
     public String defaultEndpoint() { 
         return "Hiii!";
     }
+    
     @GetMapping("/restaurants")
     public List<String> restaurantNames() {
         List<String> restaurants = new ArrayList<>();

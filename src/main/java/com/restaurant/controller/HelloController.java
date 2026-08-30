8eeb75e561d3d8b3eedc3d40050ac6f24b905566
Sahay-Snehil <strange.snehil22@icloud.com>
2026-08-30 22:36:13 +0530

    Added restaurant endpoint

--- src/main/java/com/restaurant/controller/HelloController.java
+++ src/main/java/com/restaurant/controller/HelloController.java
@@ -1,5 +1,9 @@
 package com.restaurant.controller;
 
+import java.util.List;
+import java.util.ArrayList;
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,19 @@ public class HelloController {
     public String hello() {
         return "Hello World!";
     }
+    @GetMapping("/")
+    public String defaultEndpoint() { 
+        return "Hiii!";
+    }
+    @GetMapping("/restaurants")
+    public List<String> restaurantNames() {
+        List<String> restaurants = new ArrayList<>();
+        restaurants.add("Dominos");
+        restaurants.add("Pizza Hut");
+        restaurants.add("LaPino's Pizza");
+        restaurants.add("ChicagoPizza");
+        
+        return restaurants;
+        }
+
 }
